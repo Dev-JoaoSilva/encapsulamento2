57611d4518c27d45859b18e8d103df2126e40d76
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-04-11 13:50:05 +0000

    Salvar

---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal{
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Lunna");
+        System.out.println(pessoa.getNome());
+        pessoa.setIdade(17);
+        System.out.println(pessoa.getIdade());
+    }
+}
